7b82a4ce383e86ce3f0f807e121cfeda8acff418
Alfonso Esau Gomez <alfonsoesaugomez@gmail.com>
2024-03-21 02:54:13 -0600

    docs: getJosephusPermutation comments

--- Josephus_permutation_operation.ts
+++ Josephus_permutation_operation.ts
@@ -1,10 +1,13 @@
 function getJosephusPermutation(numberValues: number[], k: number): number {
+    // Validación de escenaios que no aplican a la solución
     if (numberValues.length <= 1 || k <= 1) return 0;
 
+    // Generando copia para conservar el array original
     let currentValues: number[] = [...numberValues];
     let delIndex = 0;
 
     while (currentValues.length > 1) {
+        // Calculando indice a eliminar y simulando el recorrido en circulo mediante el operador MOD
         delIndex = (delIndex + k - 1) % currentValues.length;
         currentValues.splice(delIndex, 1);
     }
